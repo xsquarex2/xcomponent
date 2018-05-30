769ffa0c07531a85de88e1372b5e387f05baf600
574760618@qq.com <w0281225>
2018-05-30 16:53:38 +0800

    修改

--- pages/xrefresh/xrefresh.js
+++ pages/xrefresh/xrefresh.js
@@ -5,62 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    lists: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+
+  },
+  getTen() {
+    var lists = []
+    for (var i = 0; i < 10; i++) {
+      lists.push(i)
+    }
+    return lists;
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 加载更多
+  onReachBottom: function (e) {
+    var lists = this.data.lists;
+    lists.splice(this.data.lists.length, 0, 0,1,2,3,4);
+    this.setData({
+      lists: lists
+    })
   }
 })
